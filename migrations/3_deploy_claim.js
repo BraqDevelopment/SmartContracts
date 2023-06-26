16dd6152273117c7d4dba7ad2629a83656ed2fc5
Cosmodude <vladislav.lenskii@gmail.com>
2023-06-26 11:06:23 +0700

    deployment scripts

--- migrations/3_deploy_claim.js
+++ migrations/3_deploy_claim.js
@@ -1,8 +1,8 @@
 const MonstersClaim = artifacts.require("MonstersClaim");
 const FriendsClaim = artifacts.require("FriendsClaim");
 const TOKEN_CONTRACT_ADDRESS = "0x75bA3ec8A8163C35c4061BD13A17Ef13F812cAc1";
-const BraqMonstersContractAddress = "0x4A8A584FdD48d157Ad851B172D7775C3800A190f";
-const BraqFriendsContractAddress = "0x4A8A584FdD48d157Ad851B172D7775C3800A190f";
+const BraqMonstersContractAddress = "0x1D3279c7E6fdeBFCcbaAcaF9366ed7cf5b3e746a";
+const BraqFriendsContractAddress = "0x1D3279c7E6fdeBFCcbaAcaF9366ed7cf5b3e746a";
 const listingsAddress = "0x23FcC07b3286b37440988D95714952Bd3108Aa61";
 const marketingAddress = "0xce693C85a4C2c8362eb85Af9dAdc91E6A4040378";
 
