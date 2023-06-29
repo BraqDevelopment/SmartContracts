005e4c30c54f003d49da09ecc387f45911646267
Cosmodude <vladislav.lenskii@gmail.com>
2023-06-29 14:28:29 +0900

    contracts tested on testnet work well

--- contracts/FriendsClaim.sol
+++ contracts/FriendsClaim.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IBraqFriends {
