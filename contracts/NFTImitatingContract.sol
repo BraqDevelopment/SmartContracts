b5295dc7aa6eb58246414fbede92c34dc6f1148b
Cosmodude <vladislav.lenskii@gmail.com>
2023-07-10 15:10:24 +0900

    added doxygen comments

--- contracts/NFTImitatingContract.sol
+++ contracts/NFTImitatingContract.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+/// @author Vladislav Lenskii
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TestNFT is ERC721, Ownable {
-    uint256 public publicSaleSupply = 3750000;
 
-    constructor() ERC721("TestBRAQ", "TST") {}
-        
+    constructor() ERC721("TestNFT", "TST") {}
+    /**    
+     * @notice Minting function 
+     * @param _to any address that can hold NFTs
+     * @param _id Id of the NFT to be minted
+     * @dev Check if NFT is minted using the ownerOf method in "Read Contract" section
+     */
     function mint(
     address _to,
     uint256 _id 
